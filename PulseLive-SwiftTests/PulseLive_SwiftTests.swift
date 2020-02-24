6defde12484fe51a7665f2711c79dc08cb0cf84c
Inaldo Ramos Ribeiro <inaldor@bibimoney.com>
2020-02-24 15:17:53 +0000

    Initial Commit

---
+++ PulseLive-SwiftTests/PulseLive_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  PulseLive_SwiftTests.swift
+//  PulseLive-SwiftTests
+//
+//  Created by Inaldo Ramos Ribeiro on 24/02/2020.
+//  Copyright © 2020 Inaldo's Software Development. All rights reserved.
+//
+
+import XCTest
+@testable import PulseLive_Swift
+
+class PulseLive_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
